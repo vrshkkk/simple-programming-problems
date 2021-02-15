96541bb7cdec547e587a5926334d42582715fcb8
Anna Vereshak <annavereshak257@gmail.com>
2021-02-15 16:16:08 +0200

    add number of tries

--- elementary/problem9.js
+++ elementary/problem9.js
@@ -1,4 +1,4 @@
-// Write a guessing game where the user has to guess a secret number. After every guess the program tells the user whether their number was too large or too small. At the end the number of tries needed should be printed. It counts only as one try if they input the same number multiple times consecutively.
+// Write a guessing game where the user has to guess a secret number. After every guess the program tells the user whether their number was too large or too small. At the end the number of tries needed should be printed. 
 
 const ask = require("async-ask");
 function getRandom(min, max) {
@@ -13,20 +13,24 @@ async function main() {
     const min = 0;
 
     let rightNumber = getRandom(min, range);
-    console.log(rightNumber)
-    
-     let attempt = Number(await ask('Thanks!\nNow try to guess the secret number in a range ' + min + '- ' + range + ':\n'));
+
+    let attempt = Number(await ask('Thanks!\nNow try to guess the secret number in a range ' + min + '- ' + range + ':\n'));
+
+    let numberOfTries = 0;
 
     while (attempt !== rightNumber) {
         if (attempt < rightNumber) {
-            attempt = Number(await ask('Your number is too small. Try again.\n')); 
+            attempt = Number(await ask('Your number is too small. Try again.\n'));
+            numberOfTries++;
+
         }
         else if (attempt > rightNumber) {
             attempt = Number(await ask('Your number is too big. Try again.\n'));
+            numberOfTries++;
         }
     }
-
-    console.log('Congratulations! You win :)');
+    numberOfTries++;
+    console.log('Congratulations! You win :)\nTries:', numberOfTries);
 }
 
 
